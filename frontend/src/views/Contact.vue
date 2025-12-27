0671d7e170f0f70264d65b328e8dca887d0e319e
Milad Dhs <dhs.milad@gmail.com>
2025-12-27 15:50:20 +0330

    a

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -241,7 +241,7 @@ const contactCards = computed(() => [
     icon: 'fa-phone',
     title: t('contact.phoneTitle'),
     subtitle: t('contact.phoneSubtitle'),
-    lines: ['+98 901 368 4856', t('contact.phoneHours')]
+    lines: ['+989013684856', t('contact.phoneHours')]
   },
   {
     icon: 'fa-envelope',
